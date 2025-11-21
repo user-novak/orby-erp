d7dc60f989bba53a48b6adc3c2018a6156f6f4f5
novak-dev <novakmzv@gmail.com>
2025-11-21 00:23:56 -0500

    feat: se maqueto la sección 1 del facturador

--- src/app/biller/biller.ts
+++ src/app/biller/biller.ts
@@ -3,13 +3,17 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-biller',
   providers: [...provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule,MatSelectModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './biller.html',
   styleUrl: './biller.css',
 })
-export class Biller {}
+export class Biller {
+  clients: string[] = ['Cliente 1', 'Cliente 2', 'Cliente 3'];
+  clientTypes: string[] = ['Tipo 1', 'Tipo 2'];
+}
