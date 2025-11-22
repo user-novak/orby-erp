f018aac3a04ba814c2cb7d5cf61e409cb1965889
novak-dev <novakmzv@gmail.com>
2025-11-21 20:20:03 -0500

    feat: se creo primer formulario

--- src/app/biller/biller.ts
+++ src/app/biller/biller.ts
@@ -1,15 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from '../utils/ErrorStateMatcher.class';
 
 @Component({
   selector: 'app-biller',
   providers: [...provideNativeDateAdapter()],
   imports: [
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
@@ -24,6 +27,28 @@ export class Biller {
   clients: string[] = ['Cliente 1', 'Cliente 2', 'Cliente 3'];
   clientTypes: string[] = ['Tipo 1', 'Tipo 2'];
   saleTypes: string[] = ['Tipo 1', 'Tipo 2'];
-  accountTypes: string[] = ['Entidad 1', 'Entidad 2'];
+  accounts: string[] = ['Entidad 1', 'Entidad 2'];
   mesaureUnities: string[] = ['kg', 'mg'];
+
+  generalInfo: FormGroup;
+
+  matcher = new MyErrorStateMatcher();
+
+  private readonly _fb = inject(FormBuilder);
+
+  constructor() {
+    this.generalInfo = this.setGeneralInfoForm();
+  }
+
+  private setGeneralInfoForm() {
+    return this._fb.group({
+      registerDate: ['', [Validators.required]],
+      clientType: ['', [Validators.required]],
+      client: ['', [Validators.required]],
+      saleType: ['', [Validators.required]],
+      account: ['', [Validators.required]],
+      place: [''],
+      paymentDate: [''],
+    });
+  }
 }
