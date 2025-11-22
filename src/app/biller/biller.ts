4f9b0f549388686e087674c5a829b5546d814efa
novak-dev <novakmzv@gmail.com>
2025-11-22 13:20:29 -0500

    feat: se termino de maquetar el billerForm

--- src/app/biller/biller.ts
+++ src/app/biller/biller.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
@@ -10,6 +10,7 @@ import { MyErrorStateMatcher } from '../core/classess/ErrorStateMatcher';
 import { SALES_TYPES } from './constants/fields';
 import { Option } from '../core/models/global';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { BillerForm } from './models/biller';
 
 @Component({
   selector: 'app-biller',
@@ -26,7 +27,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   templateUrl: './biller.html',
   styleUrl: './biller.css',
 })
-export class Biller implements OnInit {
+export class Biller {
   saleTypes: Option[] = SALES_TYPES;
   clients: string[] = ['Cliente 1', 'Cliente 2', 'Cliente 3'];
   clientTypes: string[] = ['Tipo 1', 'Tipo 2'];
@@ -37,55 +38,72 @@ export class Biller implements OnInit {
   generalInfoForm: FormGroup;
   billerForm: FormGroup;
 
+  productsList: BillerForm[] = [];
+
   matcher = new MyErrorStateMatcher();
 
-  private readonly destroyRef = inject(DestroyRef);
   private readonly _fb = inject(FormBuilder);
 
   constructor() {
     this.generalInfoForm = this.setGeneralInfoForm();
     this.billerForm = this.setBillerForm();
+    this.disabledBillerFields();
+    this.handleSaleTypeChanges();
   }
 
-  ngOnInit(): void {
-    this.handleSaleTypeChanges();
+  addProduct() {
+    if (this.billerForm.invalid) {
+      this.billerForm.markAllAsTouched();
+      return;
+    }
+    this.productsList.push(this.billerForm.value);
+    this.resetBillerForm();
+  }
+
+  resetBillerForm() {
+    this.billerForm.reset();
   }
 
   private handleSaleTypeChanges() {
     const saleTypeControl = this.generalInfoForm.get('saleType')!;
     const paymentDateControl = this.generalInfoForm.get('paymentDate')!;
 
-    saleTypeControl.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((value) => {
+    saleTypeControl.valueChanges.pipe(takeUntilDestroyed()).subscribe((value) => {
       if (value === 'ACR') {
         paymentDateControl.setValidators([Validators.required]);
       } else {
         paymentDateControl.clearValidators();
-        paymentDateControl.setValue('');
+        paymentDateControl.setValue(null);
       }
 
       paymentDateControl.updateValueAndValidity({ emitEvent: false });
     });
   }
 
+  private disabledBillerFields(){
+    this.billerForm.get('unitPrice')?.disable();
+    this.billerForm.get('totalPrice')?.disable();
+  }
+
   private setGeneralInfoForm() {
     return this._fb.group({
-      registerDate: ['', [Validators.required]],
-      clientType: ['', [Validators.required]],
-      client: ['', [Validators.required]],
-      saleType: ['', [Validators.required]],
-      account: ['', [Validators.required]],
-      place: [''],
-      paymentDate: [''],
+      registerDate: [null, [Validators.required]],
+      clientType: [null, [Validators.required]],
+      client: [null, [Validators.required]],
+      saleType: [null, [Validators.required]],
+      account: [null, [Validators.required]],
+      place: [null],
+      paymentDate: [null],
     });
   }
 
   private setBillerForm() {
     return this._fb.group({
-      quantity: [0, [Validators.required]],
-      measureUnity: ['', [Validators.required]],
-      productDescription: ['', [Validators.required]],
-      unitPrice: [0, [Validators.required]],
-      totalPrice: [0, [Validators.required]],
+      quantity: [null, [Validators.required, Validators.min(1)]],
+      measureUnity: [null, [Validators.required]],
+      productDescription: [null, [Validators.required]],
+      unitPrice: [null, [Validators.required, Validators.min(1)]],
+      totalPrice: [null, [Validators.required, Validators.min(1)]],
     });
   }
 }
