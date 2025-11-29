5a85169f966ad8ddab6c5a7e2ee616f7c6d9c535
novak-dev <novakmzv@gmail.com>
2025-11-28 23:21:14 -0500

    feat: se maqueto tabla

---
+++ src/app/storage/models/storage.ts
@@ -0,0 +1,9 @@
+export interface StorageItem {
+    code: string;
+    description: string;
+    brand: string;
+    measure: string;
+    unit_price: number;
+    general_price: number;
+    stock: number;
+}
