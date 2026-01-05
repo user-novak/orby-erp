ee172f5927a3209dd02bb6b5d4c859f887bdeb88
novak-dev <novakmzv@gmail.com>
2026-01-04 23:50:10 -0500

    feat: finalizacion de botones en formulario de storage

--- src/app/storage/storage-create/storage-create.ts
+++ src/app/storage/storage-create/storage-create.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { StorageForm } from '../storage-form/storage-form';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-storage-create',
-  imports: [StorageForm],
+  imports: [StorageForm, MatButtonModule, MatIconModule],
   templateUrl: './storage-create.html',
   styleUrl: './storage-create.css',
 })
-export class StorageCreate {}
+export class StorageCreate {
+  private readonly router = inject(Router);
+
+  goHome() {
+    this.router.navigate(['/storage']);
+  }
+}
