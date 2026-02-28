59103769af70fab09258cab35c2962b9ea690967
novak-dev <novakmzv@gmail.com>
2026-02-27 21:27:45 -0500

    feat: se implemento funcion para editar cliente

--- src/app/clients/client-edit/client-edit.ts
+++ src/app/clients/client-edit/client-edit.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { NotificationService } from '../../core/services/notification/notification';
 import { ClientService } from '../services/client';
-import { Client } from '../models/client';
+import { Client, ClientFormValues } from '../models/client';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ApiResponse, NotificationData } from '../../core/models/global';
 import { ClientForm } from '../client-form/client-form';
@@ -33,6 +33,23 @@ export class ClientEdit implements OnInit {
 
   goClients() {
     this.router.navigate(['/clients']);
+  }
+
+  editClient(clientFormValue: ClientFormValues) {
+    this.clientService
+      .updateClient(this.clientId, clientFormValue)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/clients']);
+        },
+        error: () => {
+          this.showNotification(
+            this.generateNotification('Error al actualizar cliente', 'error', '#f44336'),
+            3000,
+          );
+        },
+      });
   }
 
   private loadClient() {
